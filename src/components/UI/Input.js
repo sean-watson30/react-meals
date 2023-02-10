5863fbc9c1225d52f909a489d67b40a1548f03b8
Sean Watson <sean_watson30@me.com>
2023-02-10 16:54:28 -0700

    adds a Cart and a Modal component

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -3,7 +3,7 @@ import classes from './Input.module.css'
 const Input = props => {
   return (
     <div className={ classes.input }>
-      <label htmlFor={ props.input.id } for>{ props.label }</label>
+      <label htmlFor={ props.input.id }>{ props.label }</label>
       <input id={ props.input.id } {...props.input} />
     </div>
   )
