679669fffcc1c75f74b411f61a8153c302d077b4
Sean Watson <sean_watson30@me.com>
2023-02-10 17:08:05 -0700

    adds state to show/hide cart

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = props => {
       <header className={ classes.header }>
         <h1>ReactMeals</h1>
         {/* <button>Cart</button> */}
-        <HeaderCartButton />
+        <HeaderCartButton onClick={ props.onShowCart } />
       </header>
       <div className={ classes['main-image']}>
         <img src={ mealsImage } alt="A table full of delicious food!" />
